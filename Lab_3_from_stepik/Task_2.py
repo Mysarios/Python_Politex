2b0f85d79fafef6961f15377b7a9532ff1610e96
Dima <99542088+Mysarios@users.noreply.github.com>
2023-10-30 00:59:14 +0300

    Add new version 3 lab
    Наладил связь пайчарм-степик и взял данные оттуда

---
+++ Lab_3_from_stepik/Task_2.py
@@ -0,0 +1,48 @@
+def find_common_participants(first_str, second_str, separator=","):
+    result = []
+
+    first_list = []
+    buffer_name = ""
+    for i in range(0, len(first_str)):
+        if first_str[i] == separator:
+            first_list.append(buffer_name)
+            buffer_name = ""
+        else:
+            buffer_name += first_str[i]
+    first_list.append(buffer_name)
+
+    second_list = []
+    buffer_name = ""
+    for i in range(0, len(second_str)):
+        if second_str[i] == separator:
+            second_list.append(buffer_name)
+            buffer_name = ""
+        else:
+            buffer_name += second_str[i]
+    second_list.append(buffer_name)
+
+    # для перебора меньшего списка и ускорения тем самым процесса
+    smallest_list = []
+    biggest_list = []
+    if len(first_list) > len(second_list):
+        smallest_list = second_list
+        biggest_list = first_list
+    else:
+        smallest_list = first_list
+        biggest_list = second_list
+
+    for person in smallest_list:
+        if biggest_list.count(person):
+            result.append(person)
+
+    return result
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+print(find_common_participants(participants_first_group,participants_second_group,"|"))
+
+participants_first_group = "Иванов,Петров,Сидоров"
+participants_second_group = "Петров,Сидоров,Смирнов"
+
+print(find_common_participants(participants_first_group,participants_second_group))
